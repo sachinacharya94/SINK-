44002990b7316ded9c50b4964ebeb704add8ac51
Sachin Acharya <96819739+sachinacharya94@users.noreply.github.com>
2024-06-03 13:42:27 +0545

    Update connection.js

--- backend/Database/connection.js
+++ backend/Database/connection.js
@@ -1,9 +1,1 @@
-const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATA_BASE)
-.then(()=>{
-    console.log("Database Connected Successfully")
-})
-.catch(
-    error=>console.log(error.message)
-)
